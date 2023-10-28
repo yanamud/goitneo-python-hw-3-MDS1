fc3f68b6fcc7269d40b884cc7a705d057dbd46b3
yanamud <atlantidas84@gmail.com>
2023-10-28 12:15:58 +0300

    updated command "add"

--- task_1.py
+++ task_1.py
@@ -4,26 +4,31 @@ from datetime import datetime
 from collections import defaultdict
 import calendar
 
+
 class Field:
     def __init__(self, value):
         self.value = value
 
     def __str__(self):
         return str(self.value)
-    
+
+
 class Birthday(Field):
     def __init__(self, value):
         super().__init__(value)
+
 
 class Name(Field):
     def __init__(self, value):
         super().__init__(value)
 
+
 class Phone(Field):
     def __init__(self, value):
         if not all((len(value) == 10, value.isdigit())):
-            raise ValueError("The number should contain 10 digits") 
+            raise ValueError("The number should contain 10 digits")
         super().__init__(value)
+
 
 class Record:
     def __init__(self, name, birthday=None, phone=None):
@@ -33,7 +38,7 @@ class Record:
 
     def add_birthday(self, birthday):
         self.birthday = birthday
-        return f"Birthday {birthday} add success to contact {self.name}"    
+        return f"Birthday {birthday} add success to contact {self.name}"
 
     def add_phone(self, phone):
         self.phones.append(Phone(phone))
@@ -47,22 +52,23 @@ class Record:
         else:
             return "The entered phone was not found"
 
-    def edit_phone(self, old_phone,new_phone):
+    def edit_phone(self, old_phone, new_phone):
         for p in self.phones:
             if p.value == Phone(old_phone):
-                self.phones.insert(self.phones.index(p)+1,Phone(new_phone))
+                self.phones.insert(self.phones.index(p)+1, Phone(new_phone))
                 del self.phones[self.phones.index(p)]
             print('the phone was changed')
-        #else:
-            #print("The entered phone was not found")
-               
+        # else:
+            # print("The entered phone was not found")
+
     def find_phone(self, found_phone):
         for p in self.phones:
             if p.value == found_phone:
                 return found_phone
 
     def __str__(self):
-       return f"Contact name: {self.name}, birthday: {self.birthday}, phones: {'; '.join(p.value for p in self.phones)}"
+        return f"Contact name: {self.name}, birthday: {self.birthday}, phones: {'; '.join(p.value for p in self.phones)}"
+
 
 class AddressBook(UserDict):
 
@@ -71,10 +77,9 @@ class AddressBook(UserDict):
         cmd = cmd.strip().lower()
         return cmd, *args
 
-
     def add_record(self, record: Record):
         self.data[record.name.value] = record
-    
+
     def find(self, name):
         if name in self.data:
             return self.data[name]
@@ -87,19 +92,19 @@ class AddressBook(UserDict):
             return "The record was removed"
         else:
             return "The entered name was not found"
-        
+
     def get_birthdays_per_week(self):
 
         dic = defaultdict(list)
-        today = datetime.today().date()   
+        today = datetime.today().date()
         for el in self.data:
-            res = str(self.data[el]).replace(',','').split()
+            res = str(self.data[el]).replace(',', '').split()
             name = res[2]
             birthday = str(res[4]).split('.')
             bd = datetime(int(birthday[2]), int(birthday[1]), int(birthday[0]))
             birthday = bd.date()
-        
-            birthday_this_year = birthday.replace(year = today.year)
+
+            birthday_this_year = birthday.replace(year=today.year)
 
             if birthday_this_year < today:
                 birthday_this_year = birthday.replace(year=today.year+1)
@@ -107,16 +112,17 @@ class AddressBook(UserDict):
             delta_days = (birthday_this_year - today).days
 
             if delta_days < 7:
-                if birthday_this_year.weekday() in (5,6):
-                    day =  "0" + "_" + calendar.day_name[0]
+                if birthday_this_year.weekday() in (5, 6):
+                    day = "0" + "_" + calendar.day_name[0]
                 else:
-                    day = str(birthday_this_year.weekday()) + "_" + calendar.day_name[birthday_this_year.weekday()]
+                    day = str(birthday_this_year.weekday()) + "_" + \
+                        calendar.day_name[birthday_this_year.weekday()]
                 dic[day].append(name)
 
         for key, value in sorted(dic.items()):
             new_key = key[2:]
             new_value = ", ".join(value)
-            print(f"{new_key}: {new_value}")     
+            print(f"{new_key}: {new_value}")
 
     def main():
 
@@ -128,26 +134,26 @@ class AddressBook(UserDict):
                         "  - 'close' or 'exit' to end the chat\n" \
                         "----------------------------------\n" \
                         "                                  \n"
-        
+
         help_message = "How can I help you?\n" \
-                        "----------------------------------\n" \
-                        "Please, enter a command:\n" \
-                        "  - 'add [username] [phone]' to adde Contact\n" \
-                        "  - 'change [username] [old phone number] [new phone number]' to update Contact\n" \
-                        "  - 'phone [username]' to get the phone number\n" \
-                        "  - 'all' to output all the data\n" \
-                        "  - 'add-birthday [username] [birthday]' to adde birthday \n" \
-                        "  - 'show-birthday [username]' to get birthday \n" \
-                        "  - 'birthdays' to display all birthdays within a week \n" \
-                        "----------------------------------\n" \
-                        "                                  \n" \
+            "----------------------------------\n" \
+            "Please, enter a command:\n" \
+            "  - 'add [username] [phone]' to adde Contact\n" \
+            "  - 'change [username] [old phone number] [new phone number]' to update Contact\n" \
+            "  - 'phone [username]' to get the phone number\n" \
+            "  - 'all' to output all the data\n" \
+            "  - 'add-birthday [username] [birthday]' to adde birthday \n" \
+            "  - 'show-birthday [username]' to get birthday \n" \
+            "  - 'birthdays' to display all birthdays within a week \n" \
+            "----------------------------------\n" \
+            "                                  \n" \
 
         print(greet_message)
 
         while True:
             user_input = input("Enter a command: ")
             command, *args = AddressBook.parse_input(user_input)
-            #print(command, *args)
+            # print(command, *args)
 
             if command in ["close", "exit"]:
                 print("Good bye!")
@@ -158,39 +164,43 @@ class AddressBook(UserDict):
 
             elif command == "add":
                 record = Record(args[0])
+                for name, record in book.data.items():
+                    if name == args[0]:
+                        print("contact exists ")
+                    else:
+                        record = Record(args[0])
                 record.add_phone(args[1])
                 book.add_record(record)
-                #print(add_contact(args, contacts))
 
             elif command == "change":
                 record = book.find(args[0])
                 record.edit_phone(args[1], args[2])
-                #print(change_contact(args, contacts))        
+                # print(change_contact(args, contacts))
 
             elif command == "phone":
                 record = book.find(args[0])
                 print(record)
-                #print(show_phone(args, contacts))       
+                # print(show_phone(args, contacts))
 
             elif command == "all":
                 for name, record in book.data.items():
-                    print(name, record)             
+                    print(name, record)
 
             elif command == "add-birthday":
                 record = Record(args[0])
                 record.add_birthday(args[1])
-                book.add_record(record)  
+                book.add_record(record)
 
             elif command == "show-birthday":
                 record = book.find(args[0])
                 print(record)
 
             elif command == "birthdays":
-                book.get_birthdays_per_week()               
-                
+                book.get_birthdays_per_week()
+
             else:
                 print("Invalid command.")
 
-       
+
 if __name__ == '__main__':
     AddressBook.main()
